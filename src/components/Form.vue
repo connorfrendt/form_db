c0d750f6b5467db048d1c0aa3801c4785c7fb2b5
Connor Frendt <cjacksonfx@yahoo.com>
2021-08-03 17:41:19 -0700

    created boilerplate front end, attempting to make and attach postgres db

---
+++ src/components/Form.vue
@@ -0,0 +1,64 @@
+<template>
+    <form @submit.prevent="asdf">
+        <div>
+            <label>
+                Name:
+                <input v-model="info.name" type="textbox">
+            </label>
+        </div>
+
+        <div>
+            <label>
+                Email:
+                <input v-model="info.email" type="textbox">
+            </label>
+        </div>
+
+        <div>
+            <label>
+                Username:
+                <input v-model="info.username" type="textbox">
+            </label>
+        </div>
+
+        <button>SUBMIT</button>
+
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            info: {
+                name: '',
+                email: '',
+                username: ''
+            }
+        }
+    },
+    methods: {
+        asdf() {
+            console.log('Name: ', this.info.name,`\n`, 'Email: ', this.info.email,`\n`, 'Username: ', this.info.username);
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
